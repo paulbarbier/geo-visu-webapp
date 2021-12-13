2133aee33cf65a2fa22ccf82a607540d215a0bd0
Paul Barbier <paulb24@orange.fr>
2021-12-13 17:53:50 +0100

    fix server.py with multiples web pages

--- server.py
+++ server.py
@@ -19,8 +19,12 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/", methods = ["GET", "POST"])
-def index(image_filename = None, position_match = None):
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/geolocalizer", methods = ["GET", "POST"])
+def geolocalizer(image_filename = None, position_match = None):
     if request.method == "POST":
         if "image" not in request.files:
             flash("No image provided in the request.")
@@ -35,10 +39,10 @@ def index(image_filename = None, position_match = None):
 
             position_match = compute_match(image_filename)
 
-            return render_template("index.html", image_filename = filename, position_match = position_match)
+            return render_template("geolocalizer.html", image_filename = filename, position_match = position_match)
         else:
             flash("Wring image format. Please provide an image with PNG, JPEG or JPG format.")
-    return render_template("index.html", image_filename = image_filename, position_match = position_match)
+    return render_template("geolocalizer.html", image_filename = image_filename, position_match = position_match)
 
 @app.route("/uploads/<filename>")
 def render_image(filename):
@@ -49,12 +53,8 @@ def render_image(filename):
 def database():
     return render_template("database.html")
 
-@app.route("/geolocalizer")
-def geolocalizer():
-    return render_template("geolocalizer.html")
-
 @app.route("/about")
-deg about():
+def about():
     return render_template("about.html")
 
 if __name__ == "__main__":
